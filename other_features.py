3d74e59190760b77de9103b26cecec36acc2723e
Nazih Bissat <nbissat@seas.upenn.edu>
2018-05-03 14:29:57 -0400

    started file to analyze features other than text and images

---
+++ other_features.py
@@ -0,0 +1,13 @@
+import code
+
+train = code.pdf.get_train_file()
+test = code.pdf.get_test_file()
+
+def extract_date(df):
+    df['created year'] = 0
+    df['created month'] = 0
+    df['created year'] = df['created'].apply(lambda x: int(str(x)[0:4]))
+    df['created month'] = df['created'].apply(lambda x: int(str(x)[5:7]))
+    return df
+
+print(extract_date(train).head())
