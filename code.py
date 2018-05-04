1250e1571c716242d0fe3f0dd13599c1a84af0f8
Ali Kozlu <akozlu1@gmail.com>
2018-05-04 16:14:27 -0400

    Panda Data Frame Class
    Import code.py as pd and call it:
    
    pd = PandaFrames('similar-staff-picks-challenge-clips.csv')
    
    pd.get_train_file() gives training data as panda dataframe
    
    pd.get_test_file() gives test data as panda dataframe

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib.request
 
 
 def load_train_and_test_files(file_path):
@@ -10,6 +13,22 @@ def load_train_and_test_files(file_path):
     train = categories[indices]
     test = categories[~indices]
     return [train, test]
+
+
+def url_is_alive(url):
+    """
+    Checks that a given URL is reachable.
+    :param url: A URL
+    :rtype: bool
+    """
+    request = urllib.request.Request(url)
+    request.get_method = lambda: 'HEAD'
+
+    try:
+        urllib.request.urlopen(request)
+        return True
+    except urllib.request.HTTPError:
+        return False
 
 
 class PandaFrames(object):
@@ -25,8 +44,63 @@ class PandaFrames(object):
     def get_test_file(self):
         test = self.pandaframes[1]
         test = test.reset_index()
-        train = test.drop(columns=['Unnamed: 0', 'index'])
+        test = test.drop(columns=['Unnamed: 0', 'index'])
         return test
+
+    def get_description_for_train_file(self):
+        train = self.get_train_file()
+
+        for id in train['id']:
+
+            url = 'https://vimeo.com/' + str(id)
+            url_valid = url_is_alive(url)
+
+            if url_valid:  # check if url is valid
+                vimeo_webpage = urlopen(url)
+
+                content = vimeo_webpage.read()  # get content from webpage
+                soup = BeautifulSoup(content, 'html.parser')
+
+                if soup.find('div', attrs={
+                    'class': 'clip_details-description description-wrapper iris_desc'}) is not None:
+                    # get the video description (all paragraphs instead of first paragraph
+                    article_soup = [s.get_text(separator=" ", strip=True) for s in soup.find('div', attrs={
+                        'class': 'clip_details-description description-wrapper iris_desc'}).find_all(
+                        'p')]
+
+                    index = train.loc[train['id'] == id].index[0]
+
+                    print(' '.join(article_soup))
+
+                    train.loc[index, 'caption'] = ' '.join(article_soup)  # update the caption of our train dataset
+
+        def get_description_for_test_file(self):
+            test = self.get_test_file()
+
+            for id in test['id']:
+
+                url = 'https://vimeo.com/' + str(id)
+                url_valid = url_is_alive(url)
+
+                if url_valid:  # check if url is valid
+                    vimeo_webpage = urlopen(url)
+
+                    content = vimeo_webpage.read()  # get content from webpage
+                    soup = BeautifulSoup(content, 'html.parser')
+
+                    if soup.find('div', attrs={
+                        'class': 'clip_details-description description-wrapper iris_desc'}) is not None:
+                        # get the video description (all paragraphs instead of first paragraph)
+                        article_soup = [s.get_text(separator=" ", strip=True) for s in soup.find('div', attrs={
+                            'class': 'clip_details-description description-wrapper iris_desc'}).find_all(
+                            'p')]
+
+                        index = test.loc[test['id'] == id].index[0]
+
+                        # print(' '.join(article_soup))
+
+                        test.loc[index, 'caption'] = ' '.join(article_soup)  # update the caption of our train dataset
 
 
 pdf = PandaFrames('similar-staff-picks-challenge-clips.csv')
+pdf.get_description()
