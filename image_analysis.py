2e79b89d395498f4256ac0201be70b6051a80dbd
Christian Duffy de Abreu <christianduffydeabreu@Christians-MacBook-Pro.local>
2018-05-04 19:05:03 -0400

    lads

--- image_analysis.py
+++ image_analysis.py
@@ -1,45 +1,76 @@
-from PIL import Image
+# If importing images for the first time, save them as jpegs into folder (no need to right now since we already have them)
+# from PIL import Image
 
-for thumbnail in os.listdir(directory):
-    if '.DS_Store' in thumbnail:
-        continue
-    im = Image.open(thumbnail).convert("RGB")
-    im.save("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg/" + thumbnail[
-                                                                                                            :-5] + ".jpg",
-            "jpeg")
-
+# import os
+# directory = os.chdir("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project/")
+# for thumbnail in os.listdir(directory):
+#    if '.DS_Store' in thumbnail:
+#        continue
+#    im = Image.open(thumbnail).convert("RGB")
+#    im.save("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg/"
+#            + thumbnail[:-5] + ".jpg", "jpeg")
 # %%Converting pics to gracyscale
+import os
 import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.feature as feature
+import csv
+import pandas as pd
+from scipy.misc import imread
+from scipy.linalg import norm
+from scipy import sum, average
+import numpy as np
+from sklearn.cluster import KMeans
+import operator
+from itertools import *
 
 
+# %% Saving Images to New Gray Form
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
 
 gray_images = []
-for image in os.listdir("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg/"):
+reg_images = []
+# Saving Gray Images to List
+dir_path = "/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg"
+# dir_path = os.path.dirname(os.path.realpath('__file__'))
+count = 0
+for image in os.listdir(dir_path):
     if '.DS_Store' in image:
         continue
     img = mpimg.imread(image)
-    gray_images.append(rgb2gray(img))
+    gray_images.append(list(rgb2gray(img)))
+    reg_images.append(image)  # names of files
+    count += 1
+    print(count)
 
-# %% Reshaping
-import matplotlib.image as mpimg
-import skimage.feature as feature
-
+# Saving Gray Images to CSV in Directory --> not working right now
+with open('gray_ims.csv', "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerows(gray_images)
+# %% Reshaping Features for Distance Measure (Slightly Different Processing)
 img_converted = []
-# for idx, image in enumerate(os.listdir("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg/")):
-for i, image in enumerate(gray_images):
+gray_ims = pd.read_csv('gray.csv')
+gray_ims = gray_ims.iloc[:, 1]
+for i in range(len(gray_images)):
+    image = mpimg.imread(gray_images[i])
     img_converted.append(feature.hog(image))
     print(i)
 
+
 # %% PCA Min Transform
-from scipy.misc import imread
-from scipy.linalg import norm
-from scipy import sum, average
+
+# %%
+###############################################################################
 
 
-# %% Normalizing images
+#                            Interacting With Images                          #
+
+
+###############################################################################
+# %% Comparitive Functions
 def normalize(arr):
     rng = arr.max() - arr.min()
     amin = arr.min()
@@ -59,11 +90,50 @@ def compare_images(img1, img2):
 
 
 # %% Running Clustering taking euclidean distance in space
-import numpy as np
-from sklearn.cluster import KMeans
-import operator
-# from scipy.stats import nanmean
-from itertools import *
-
+# Clustering
 clustermdl = KMeans(n_clusters=90, random_state=40)
 clustermdl.fit(gray_images)
+
+
+# %% Taking Pairwise Distance Between Images
+def compare_all(clip_id, images, k):
+    m_norm = []
+    z_norm = []
+    test_image = images.index(clip_id)
+    for item in enumerate(images):
+        if item == test_image:
+            continue
+        m_norm, z_norm = compare_images(test_image, item)
+
+    dist_m = pd.DataFrame.from_items(m_norm)
+    dist_z = pd.DataFrame.from_items(z_norm)
+    frames = [dist_m dist_z]
+    scores = pd.concat(frames)
+    return top_k, scores
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
