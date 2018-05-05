9b26e0fb532a3e4510b10926873c0e762368924b
christianduffydeabreu <christianduffydeabreu@gmail.com>
2018-05-05 15:20:10 -0400

    Merge branch 'master' of https://github.com/akozlu/419Final
    # Conflicts:
    #	image_analysis.py

--- image_analysis.py
+++ image_analysis.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 '''
 What you need in your directory:
     similar-similar-staff-picks-challenge-clips.csv
@@ -9,62 +7,35 @@ Running Code Will:
     from urls scraped.
 '''
 # %%Converting pics to gracyscale
-import os
+
+from PIL import Image
+from scipy.misc import imread
+from scipy.linalg import norm
+from scipy import sum, average
+from sklearn.cluster import KMeans
+
 import matplotlib.image as mpimg
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage.feature as feature
 import csv
 import pandas as pd
-from scipy.misc import imread
-from scipy.linalg import norm
-from scipy import sum, average
-import numpy as np
-from sklearn.cluster import KMeans
-import operator
-from itertools import *
-import pandas as pd
 import os
 import sys
+import numpy as np
 import feature_extraction
 
-data = pd.read_csv("similar-staff-picks-challenge-clips.csv")
-"""
-Ali Comment- When I run this file the above line gives the following error: 
-I think the line beginning of the code ensures everything is in UTF-8, but this does not apply to our project . 
-So we should maybe getting the data with:
+
+#Extracting Dataset
 pdf = feature_extraction.PandaFrames("similar-staff-picks-challenge-clips.csv")
-data = pdf.get_train_file() 
-Traceback (most recent call last):
-  File "/Users/aysekozlu/PycharmProjects/419Final/image_analysis.py", line 31, in <module>
-    data = pd.read_csv("similar-staff-picks-challenge-clips.csv")
-  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 709, in parser_f
-    return _read(filepath_or_buffer, kwds)
-  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 455, in _read
-    data = parser.read(nrows)
-  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 1069, in read
-    ret = self._engine.read(nrows)
-  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 1839, in read
-    data = self._reader.read(nrows)
-
-File "pandas/_libs/parsers.pyx", line 902, in pandas._libs.parsers.TextReader.read
-  File "pandas/_libs/parsers.pyx", line 924, in pandas._libs.parsers.TextReader._read_low_memory
-  File "pandas/_libs/parsers.pyx", line 1001, in pandas._libs.parsers.TextReader._read_rows
-  File "pandas/_libs/parsers.pyx", line 1130, in pandas._libs.parsers.TextReader._convert_column_data
-  File "pandas/_libs/parsers.pyx", line 1182, in pandas._libs.parsers.TextReader._convert_tokens
-  File "pandas/_libs/parsers.pyx", line 1281, in pandas._libs.parsers.TextReader._convert_with_dtype
-  File "pandas/_libs/parsers.pyx", line 1297, in pandas._libs.parsers.TextReader._string_convert
-  File "pandas/_libs/parsers.pyx", line 1539, in pandas._libs.parsers._string_box_utf8
-UnicodeDecodeError: 'utf-8' codec can't decode byte 0xdb in position 18: invalid continuation byte
-
-"""
-"""Maybe run the following line """
-# pdf = feature_extraction.PandaFrames("similar-staff-picks-challenge-clips.csv")
-# data = pdf.get_train_file()
-
-
-clips = data[['id', 'thumbnail']]
-# %% Scraping List of Urls
+data1 = pdf.get_train_file()
+data2 = pdf.get_test_file()
+all_data = pd.concat([data1, data2])
+clips = all_data[['id', 'thumbnail']]
+dir_path = os.path.dirname(os.path.realpath('__file__'))
+webppath = dir_path + "/WebP_Files/"
+jpgpath = dir_path + "/JPG_Files/"
+# %% Scraping List of Urls for Images and Saving Them to WebP Folder
 try:
     from urlparse import urljoin
 except ImportError:
@@ -103,13 +74,12 @@ class Scraper:
     def download_image(self, image_url):
         local_filename = image_url.split('/')[-1].split("?")[0]
         r = self.session.get(image_url, stream=True, verify=False)
-        newpath = dir_path + "/WebP_Files/"
-        if not os.path.exists(newpath):
-            os.makedirs(newpath)
-        with open(newpath + local_filename, 'wb') as f:
+        webppath = dir_path + "/WebP_Files/"
+        if not os.path.exists(webppath):
+            os.makedirs(webppath)
+        with open(webppath + local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 f.write(chunk)
-
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath('__file__'))
@@ -117,56 +87,54 @@ if __name__ == '__main__':
         scraper = Scraper()
         scraper.visit_url(image, 1)
         scraper.download_image(image)
-# %%Importing them all as jpg
-from PIL import Image
 
-dir_path = os.path.dirname(os.path.realpath('__file__'))
-newpath = dir_path + "/WebP_Files/"
-jpgpath = dir_path + "/JPG_Files/"
+#%% Importing images as jpg
+def import_jpeg():
+    dir_path = os.path.dirname(os.path.realpath('__file__'))
+    if not os.path.exists(jpgpath):
+        os.makedirs(jpgpath)
+    os.chdir(webppath)
+    for thumbnail in os.listdir(webppath):
+        if '.DS_Store' in thumbnail:
+            continue
+        """" This line gives OSError: cannot identify image file '100041054_780x439.webp ? Image.open cannot find image with id 100041054"""
+        ''' This is probably because of the paths. If you were able to download the images to a WebP Folder, then this should work. I'll try this again'''
+        im = Image.open(thumbnail).convert("RGB")
+        im.save(jpgpath + thumbnail[:-5] + ".jpg", "jpeg")
+    os.chdir(dir_path)
 
-if not os.path.exists(jpgpath):
-    os.makedirs(jpgpath)
-
-os.chdir(newpath)
-jpgpath = dir_path + "/JPG_Files/"
-for thumbnail in os.listdir(newpath):
-    if '.DS_Store' in thumbnail:
-        continue
-    """" This line gives OSError: cannot identify image file '100041054_780x439.webp ? Image.open cannot find image with id 100041054"""
-    im = Image.open(thumbnail).convert("RGB")
-    im.save(jpgpath + thumbnail[:-5] + ".jpg", "jpeg")
-os.chdir(dir_path)
-# %%Converting pics to grayscale
-os.chdir(jpgpath)
-import matplotlib.image as mpimg
-
-
+#Grayscale Conversion
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
+#Importing List of Images
+def image_list(color):
+    image_list = []
+    for image in os.listdir(jpgpath):
+        if '.DS_Store' in image: #This may sometimes be found in a folder preventing uploads
+            continue
+        img = mpimg.imread(image)
+        if color == "gray":
+            image_list.append(rgb2gray(img))
+        elif color == "rgb":
+            image_list.append(img)
+    return(image_list)
 
-gray_images = []
-rgb_images = []
-for image in os.listdir(jpgpath):
-    if '.DS_Store' in image:
-        continue
+def hog_image(color):
+    img_converted = []
+    if color == "gray":
+        for i, image in enumerate(gray_images):
+            img_converted.append(feature.hog(image))
+    if color == "rgb":
+        for k, image in enumerate(rgb_images):
+            img_converted.append(feature.hog(image))
+    return(img_converted)
 
-    img = mpimg.imread(image)
-    rgb_images.append(img)
-    gray_images.append(rgb2gray(img))
-
-# %% Reshaping Image To New Form
-import matplotlib.image as mpimg
-import skimage.feature as feature
-
-img_converted_gray = []
-img_converted_rgb = []
-for i, image in enumerate(gray_images):
-    img_converted_gray.append(feature.hog(image))
-    print(i)
-for k, image in enumerate(rgb_images):
-    img_converted_rgb.append(feature.hog(image))
-
+#Extracting List of Images
+os.chdir(jpgpath)
+import_jpeg()
+gray_images = image_list("gray")
+rgb_images = image_list("rgb")
 
 # %%
 ###############################################################################
@@ -183,36 +151,53 @@ def normalize(arr):
     return (arr - amin) * 255 / rng
 
 
-def compare_images(img1, img2):
+def compare_images(img1, img2, distance):
     # normalize to compensate for exposure difference, this may be unnecessary
     # consider disabling it
-    img1 = normalize(img1)
-    img2 = normalize(img2)
+#    img1 = normalize(img1)
+#    img2 = normalize(img2)
     # calculate the difference and its norms
     diff = img1 - img2  # elementwise for scipy arrays
-    m_norm = sum(abs(diff))  # Manhattan norm
-    z_norm = norm(diff.ravel(), 0)  # Zero norm
-    return (m_norm, z_norm)
+    if distance == "m":
+        d = sum(abs(diff))  # Manhattan norm
+    if distance == "z":
+        d = norm(diff.ravel(), 0)  # Zero norm
+    return (d)
 
-
-# %% Running Clustering taking euclidean distance in space
-# Clustering
-clustermdl = KMeans(n_clusters=90, random_state=40)
-clustermdl.fit(gray_images)
-
-"""def compare_all(clip_id, images, k):
-    m_norm = []
-    z_norm = []
-    test_image = images.index(clip_id)
-    for item in enumerate(images):
-        if item == test_image:
+#clip_id as int, image(gray or rgb), k for top returned, and 'z' or 'm' for distance type
+def compare_all(clip_id, images, k, distance, show):
+    d = []
+    test_image_index = np.where(clips['id'] == clip_id)[0][0]
+    test_image = images[test_image_index]
+    for idx, item in enumerate(images):
+        if idx == test_image_index:
+            d.append(0)
             continue
-        m_norm, z_norm = compare_images(test_image, item)
+        if np.shape(item) == (439,780):
+            item = item[1:]
+        temp = compare_images(test_image, item, distance)
+        d.append(temp)
+    ddf = pd.DataFrame(d, columns = ['Norm Distance'])
+    scores_df  = clips
+    scores_df['Norm Distance'] = d
+    top_k= scores_df.sort_values(by=['Norm Distance'])[1:k]
 
-    dist_m = pd.DataFrame.from_items(m_norm)
-    dist_z = pd.DataFrame.from_items(z_norm)
-    frames = [dist_m dist_z]
-    scores = pd.concat(frames)
-    return top_k, scores
-"""
-# %% Taking Pairwise Distance Between Images
+    if show == True:
+        indices = top_k.index.get_values()
+        for index in indices:
+            plt.figure()
+            plt.imshow(images[index])
+    return(scores_df, top_k)
+
+
+
+#Example Usage:
+clip_id = 249450406
+images = gray_images
+k = 10
+distance = 'm'
+show= True
+compare_all(clip_id, images, k, distance, show)
+
+#%%
+
