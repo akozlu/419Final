7704533633a9b18e9db19c8b7ab74f9212d10931
Ali Kozlu <akozlu1@gmail.com>
2018-05-05 00:42:52 -0400

    Check image_analysis.py for the printout of the errors I get.

--- image_analysis.py
+++ image_analysis.py
@@ -26,8 +26,43 @@ from itertools import *
 import pandas as pd
 import os
 import sys
+import feature_extraction
 
 data = pd.read_csv("similar-staff-picks-challenge-clips.csv")
+"""
+Ali Comment- When I run this file the above line gives the following error: 
+I think the line beginning of the code ensures everything is in UTF-8, but this does not apply to our project . 
+So we should maybe getting the data with:
+pdf = feature_extraction.PandaFrames("similar-staff-picks-challenge-clips.csv")
+data = pdf.get_train_file() 
+Traceback (most recent call last):
+  File "/Users/aysekozlu/PycharmProjects/419Final/image_analysis.py", line 31, in <module>
+    data = pd.read_csv("similar-staff-picks-challenge-clips.csv")
+  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 709, in parser_f
+    return _read(filepath_or_buffer, kwds)
+  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 455, in _read
+    data = parser.read(nrows)
+  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 1069, in read
+    ret = self._engine.read(nrows)
+  File "/Users/aysekozlu/anaconda3/lib/python3.6/site-packages/pandas/io/parsers.py", line 1839, in read
+    data = self._reader.read(nrows)
+
+File "pandas/_libs/parsers.pyx", line 902, in pandas._libs.parsers.TextReader.read
+  File "pandas/_libs/parsers.pyx", line 924, in pandas._libs.parsers.TextReader._read_low_memory
+  File "pandas/_libs/parsers.pyx", line 1001, in pandas._libs.parsers.TextReader._read_rows
+  File "pandas/_libs/parsers.pyx", line 1130, in pandas._libs.parsers.TextReader._convert_column_data
+  File "pandas/_libs/parsers.pyx", line 1182, in pandas._libs.parsers.TextReader._convert_tokens
+  File "pandas/_libs/parsers.pyx", line 1281, in pandas._libs.parsers.TextReader._convert_with_dtype
+  File "pandas/_libs/parsers.pyx", line 1297, in pandas._libs.parsers.TextReader._string_convert
+  File "pandas/_libs/parsers.pyx", line 1539, in pandas._libs.parsers._string_box_utf8
+UnicodeDecodeError: 'utf-8' codec can't decode byte 0xdb in position 18: invalid continuation byte
+
+"""
+"""Maybe run the following line """
+# pdf = feature_extraction.PandaFrames("similar-staff-picks-challenge-clips.csv")
+# data = pdf.get_train_file()
+
+
 clips = data[['id', 'thumbnail']]
 # %% Scraping List of Urls
 try:
@@ -97,6 +132,7 @@ jpgpath = dir_path + "/JPG_Files/"
 for thumbnail in os.listdir(newpath):
     if '.DS_Store' in thumbnail:
         continue
+    """" This line gives OSError: cannot identify image file '100041054_780x439.webp ? Image.open cannot find image with id 100041054"""
     im = Image.open(thumbnail).convert("RGB")
     im.save(jpgpath + thumbnail[:-5] + ".jpg", "jpeg")
 os.chdir(dir_path)
@@ -114,6 +150,7 @@ rgb_images = []
 for image in os.listdir(jpgpath):
     if '.DS_Store' in image:
         continue
+
     img = mpimg.imread(image)
     rgb_images.append(img)
     gray_images.append(rgb2gray(img))
@@ -163,9 +200,7 @@ def compare_images(img1, img2):
 clustermdl = KMeans(n_clusters=90, random_state=40)
 clustermdl.fit(gray_images)
 
-
-# %% Taking Pairwise Distance Between Images
-def compare_all(clip_id, images, k):
+"""def compare_all(clip_id, images, k):
     m_norm = []
     z_norm = []
     test_image = images.index(clip_id)
@@ -179,30 +214,5 @@ def compare_all(clip_id, images, k):
     frames = [dist_m dist_z]
     scores = pd.concat(frames)
     return top_k, scores
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+# %% Taking Pairwise Distance Between Images
