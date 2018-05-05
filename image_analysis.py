43b784927fed70880a2684a769792e6cb9a1434e
christianduffydeabreu <christianduffydeabreu@gmail.com>
2018-05-04 22:22:29 -0400

    eh nois

--- image_analysis.py
+++ image_analysis.py
@@ -1,14 +1,13 @@
-# If importing images for the first time, save them as jpegs into folder (no need to right now since we already have them)
-# from PIL import Image
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+What you need in your directory:
+    similar-similar-staff-picks-challenge-clips.csv
 
-# import os
-# directory = os.chdir("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project/")
-# for thumbnail in os.listdir(directory):
-#    if '.DS_Store' in thumbnail:
-#        continue
-#    im = Image.open(thumbnail).convert("RGB")
-#    im.save("/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg/"
-#            + thumbnail[:-5] + ".jpg", "jpeg")
+Running Code Will:
+    create two subdirectories: WebP_Files nad JPG_Files containing all images
+    from urls scraped.
+'''
 # %%Converting pics to gracyscale
 import os
 import matplotlib.image as mpimg
@@ -24,43 +23,113 @@ import numpy as np
 from sklearn.cluster import KMeans
 import operator
 from itertools import *
+import pandas as pd
+import os
+import sys
+
+data = pd.read_csv("similar-staff-picks-challenge-clips.csv")
+clips = data[['id', 'thumbnail']]
+# %% Scraping List of Urls
+try:
+    from urlparse import urljoin
+except ImportError:
+    from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
 
 
-# %% Saving Images to New Gray Form
+class Scraper:
+    def __init__(self):
+        self.visited = set()
+        self.session = requests.Session()
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36"}
+        requests.packages.urllib3.disable_warnings()  # turn off SSL warnings
+
+    def visit_url(self, url, level):
+        print(url)
+        if url in self.visited:
+            return
+
+        self.visited.add(url)
+
+        content = self.session.get(url, verify=False).content
+        soup = BeautifulSoup(content, "lxml")
+
+        for img in soup.select("img[src]"):
+            image_url = img["src"]
+            if not image_url.startswith(("data:image", "javascript")):
+                self.downlzoad_image(urljoin(url, image_url))
+
+        if level > 0:
+            for link in soup.select("a[href]"):
+                self.visit_url(urljoin(url, link["href"]), level - 1)
+
+    def download_image(self, image_url):
+        local_filename = image_url.split('/')[-1].split("?")[0]
+        r = self.session.get(image_url, stream=True, verify=False)
+        newpath = dir_path + "/WebP_Files/"
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        with open(newpath + local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                f.write(chunk)
+
+
+if __name__ == '__main__':
+    dir_path = os.path.dirname(os.path.realpath('__file__'))
+    for idx, image in enumerate(clips['thumbnail']):
+        scraper = Scraper()
+        scraper.visit_url(image, 1)
+        scraper.download_image(image)
+# %%Importing them all as jpg
+from PIL import Image
+
+dir_path = os.path.dirname(os.path.realpath('__file__'))
+newpath = dir_path + "/WebP_Files/"
+jpgpath = dir_path + "/JPG_Files/"
+
+if not os.path.exists(jpgpath):
+    os.makedirs(jpgpath)
+
+os.chdir(newpath)
+jpgpath = dir_path + "/JPG_Files/"
+for thumbnail in os.listdir(newpath):
+    if '.DS_Store' in thumbnail:
+        continue
+    im = Image.open(thumbnail).convert("RGB")
+    im.save(jpgpath + thumbnail[:-5] + ".jpg", "jpeg")
+os.chdir(dir_path)
+# %%Converting pics to grayscale
+os.chdir(jpgpath)
+import matplotlib.image as mpimg
+
+
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
 
 gray_images = []
-reg_images = []
-# Saving Gray Images to List
-dir_path = "/Users/christianduffydeabreu/Documents/UPenn/Senior Spring/CIS 519/Project /thumbnails_jpg"
-# dir_path = os.path.dirname(os.path.realpath('__file__'))
-count = 0
-for image in os.listdir(dir_path):
+rgb_images = []
+for image in os.listdir(jpgpath):
     if '.DS_Store' in image:
         continue
     img = mpimg.imread(image)
-    gray_images.append(list(rgb2gray(img)))
-    reg_images.append(image)  # names of files
-    count += 1
-    print(count)
+    rgb_images.append(img)
+    gray_images.append(rgb2gray(img))
 
-# Saving Gray Images to CSV in Directory --> not working right now
-with open('gray_ims.csv', "w") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    writer.writerows(gray_images)
-# %% Reshaping Features for Distance Measure (Slightly Different Processing)
-img_converted = []
-gray_ims = pd.read_csv('gray.csv')
-gray_ims = gray_ims.iloc[:, 1]
-for i in range(len(gray_images)):
-    image = mpimg.imread(gray_images[i])
-    img_converted.append(feature.hog(image))
+# %% Reshaping Image To New Form
+import matplotlib.image as mpimg
+import skimage.feature as feature
+
+img_converted_gray = []
+img_converted_rgb = []
+for i, image in enumerate(gray_images):
+    img_converted_gray.append(feature.hog(image))
     print(i)
+for k, image in enumerate(rgb_images):
+    img_converted_rgb.append(feature.hog(image))
 
-
-# %% PCA Min Transform
 
 # %%
 ###############################################################################
