dfc7f67721a32679d25f7f6872abc39c3f82866a
Ali Kozlu <akozlu1@gmail.com>
2018-05-08 19:31:32 -0400

    For text_analysis.py I removed some comments and added a simple usage command in case someone wants to run TF-idf model.
    All other change was reformatting

--- Image_Similarity.py
+++ Image_Similarity.py
@@ -16,66 +16,72 @@ import os
 import matplotlib.image as mpimg
 import pandas as pd
 from distance_transformation import *
-from image_analysis import * # For RGB
+from image_analysis import *  # For RGB
 
-#Importing Data
-all_data = ft.load_whole_file("similar-staff-picks-challenge-clips.csv", "similar-staff-picks-challenge-clip-categories.csv", "similar-staff-picks-challenge-categories.csv")             
+# Importing Data
+all_data = ft.load_whole_file("similar-staff-picks-challenge-clips.csv",
+                              "similar-staff-picks-challenge-clip-categories.csv",
+                              "similar-staff-picks-challenge-categories.csv")
 data = extract_data_with_thumbid("similar-staff-picks-challenge-clips.csv")
 
-#Removing those without Columns
-def remove_empty_categories(data): 
+
+# Removing those without Columns
+def remove_empty_categories(data):
     data = data.reindex(index=data.index[::-1])
-    rows_to_delete= []
+    rows_to_delete = []
     for i in range(len(data)):
         if data['main categories'].values[i] == []:
             rows_to_delete.append(i)
-    data.drop(data.index[rows_to_delete], inplace = True)
-    data.reset_index(inplace = True)
-    return(data)
+    data.drop(data.index[rows_to_delete], inplace=True)
+    data.reset_index(inplace=True)
+    return (data)
+
+
 all_data = remove_empty_categories(all_data)
 data = remove_empty_categories(data)
-
 
 images = vectorize_images('vect_images', data)
 dir_path = os.path.dirname(os.path.realpath('__file__'))
 webppath = dir_path + "/WebP_Files/"
 jpgpath = dir_path + "/samples/"
 
+
 def image_list(color):
     image_list = []
     ''' Saving Images to List '''
     for image in os.listdir(jpgpath):
-        if '.DS_Store' in image: #This may sometimes be found in a folder preventing uploads
+        if '.DS_Store' in image:  # This may sometimes be found in a folder preventing uploads
             continue
         img = mpimg.imread(image)
         if color == "gray":
             image_list.append(rgb2gray(img))
         elif color == "rgb":
             image_list.append(img)
-    return(image_list)
+    return (image_list)
+
+
 os.chdir(jpgpath)
 rgb_images = image_list('rgb')
 
-def image_similarity(clip_id, p = 3, all_images = rgb_images, data = data, images = images, mode = 'euclidean' , k = 10, show = False):
+
+def image_similarity(clip_id, p=3, all_images=rgb_images, data=data, images=images, mode='euclidean', k=10, show=False):
     d = []
     test_image_index = np.where(data['id'] == clip_id)[0][0]
     test_image = images[test_image_index]
-    
-    #Comparing To Every Other Image:
+
+    # Comparing To Every Other Image:
     for idx, item in enumerate(images):
         if idx == test_image_index:
             d.append(0)
             continue
         temp = ot.calculate_distance(test_image, item, mode, p)
         d.append(temp)
-    ddf = pd.DataFrame(d, columns = ['Distance'])
-    scores_df  = data
+    ddf = pd.DataFrame(d, columns=['Distance'])
+    scores_df = data
     scores_df['Distance'] = d
-    top_k= scores_df.sort_values(by=['Distance'])[1:k]
-    
-    
-    
-    #Show for plotting
+    top_k = scores_df.sort_values(by=['Distance'])[1:k]
+
+    # Show for plotting
     if show == True:
         indices = top_k.index.get_values()
         for index in indices:
@@ -84,18 +90,12 @@ def image_similarity(clip_id, p = 3, all_images = rgb_images, data = data, image
     plt.figure()
     plt.imshow(rgb_images[np.where(data['id'] == clip_id)[0][0]])
     plt.title('Original Image')
-    return(top_k)
-#%%
-#Example:
+    return (top_k)
+
+
+# %%
+# Example:
 clip_id = data['id'][1182]
 k = 10
 mode1 = 'euclidean'
-top_clips = image_similarity(clip_id = clip_id, mode = mode1, k = k, show = False)
-
-
-    
-
-
-    
-
-
+top_clips = image_similarity(clip_id=clip_id, mode=mode1, k=k, show=False)
--- predict_CNN_images.py
+++ predict_CNN_images.py
@@ -7,17 +7,18 @@ from os.path import isfile, join
 import numpy as np
 import pickle
 
+
 def cnn_imagenet_predict(folder_path):
     """ Return list of vectorized images"""
-    
-    model = ResNet50(weights='imagenet', include_top = False, pooling = 'avg')
-    image_files = [f for f in os.listdir(folder_path) if (isfile(join(folder_path,f)) and f != ".DS_Store")]    
+
+    model = ResNet50(weights='imagenet', include_top=False, pooling='avg')
+    image_files = [f for f in os.listdir(folder_path) if (isfile(join(folder_path, f)) and f != ".DS_Store")]
     images = []
-    for pic in  image_files:
-        img = image.load_img((folder_path + "/" + pic), target_size = (224,224)) 
+    for pic in image_files:
+        img = image.load_img((folder_path + "/" + pic), target_size=(224, 224))
         x = image.img_to_array(img)
-        x = np.expand_dims(x,axis=0)
+        x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         features = model.predict(x)[0]
-        images += [(pic.split("_")[0], np.char.mod('%f', features))]   
+        images += [(pic.split("_")[0], np.char.mod('%f', features))]
     return images
